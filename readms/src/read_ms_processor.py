9f60029118fef55667c4faba032b04a32497fe09
caixiuhong <caixiuhong2009@gmail.com>
2020-06-22 01:00:07 -0400

    Add read_new_ms function to read Stable-MMCCE version microstate and output lowest-energy microstate.

--- readms/src/read_ms_processor.py
+++ readms/src/read_ms_processor.py
@@ -3,6 +3,7 @@ import sys
 from mfe_adv import read_headlst, read_file_respect_comments
 import os
 import struct
+import re
 
 #store each microstate information
 class MSRECORD: 
@@ -174,6 +175,8 @@ class read_ms_processor:
 		read ms.dat file, a binary file of microstate file, into ms_states
 		input: ms.dat, format version: (/home/cai/mcce3.5)
 		output: ms_state, a list
+				lowest_state (microstate with lowest energy), a dictionary with 
+				fixed conformer id list and free conformer id list
 		'''
 		ms_start=kwargs.get("ms_start",None)
 		ms_end=kwargs.get("ms_end", None)
@@ -263,6 +266,111 @@ class read_ms_processor:
 		return self.ms_states
 
 
+	def read_new_ms(self, file_path ):
+		'''
+		read ms_out/pH#eH#ms.txt file, a txt file of microstate file, into ms_states
+		input: ms_out/pH#eHms.txt, format version: (/home/cai/Github/Stable-MCCE/bin/mcce)
+		output: ms_state, a list
+				lowest_state (microstate with lowest energy): a dictionary
+		'''
+		n_lines = 0
+		lowest_E=float('inf')
+		lowest_state={'fixed_conf_ids':[], 'free_conf_ids':[]}
+		with open(file_path, 'r') as in_file:
+			line = in_file.readline()
+			while line:
+				#opt out lines starting with "#" (comments) or blank lines
+				while (len(line.rstrip()) ==0 or line[0] == '#'): 
+					#print('delete',line)
+					line = in_file.readline()
+
+				n_lines += 1
+				#print("--",line,"--")
+
+				#read line 1: ph, eh, temp
+				if (n_lines == 1):
+					tmp= re.split(',|:', line)
+					T, pH, eH = float(tmp[1]), float(tmp[3]), float(tmp[5])
+					print('Temperature: {:.2f}, pH: {:.2f}, eH: {:.2f}'.format(T, pH, eH))
+
+				#read line 2: method
+				elif (n_lines == 2):
+					tmp=line.rstrip().split(':')
+					method = tmp[1]
+					print('Method: {}'.format(method))
+
+				#read line 3: fixed conformer ids
+				elif (n_lines == 3):
+					fixed_conf_ids=[]
+					n_fixed, fixed_conf_id_str= line.split(':')
+					n_fixed = int(n_fixed)
+					fixed_conf_ids = list(map(int, fixed_conf_id_str.split()))
+					print('n_fixed_residue: {:d}, fixed_conf_ids: {}'.format(n_fixed, fixed_conf_ids))
+					
+					if len(fixed_conf_ids) > n_fixed:
+						raise ValueError('number of fixed conf ids must not be large than n_fixed')
+
+					lowest_state['fixed_conf_ids'] = fixed_conf_ids
+
+				#read line 4: conformer ids of each free residue
+				elif (n_lines == 4):
+					conf_res_inter={}
+					n_free_check = 0
+					n_free, free_conf_id_str=line.split(':')
+					n_free = int(n_free)
+					
+					for x in free_conf_id_str.strip(';\n').split(';'):
+						for y in x.split():
+							conf = int(y)
+							conf_res_inter[conf] = n_free_check
+						n_free_check += 1
+					print('n_free: {}, free_conf_residue_iter: {}'.format(n_free, conf_res_inter))
+
+					if (n_free != n_free_check):
+						raise ValueError('number of free residues must be equal to n_free value')
+
+				#read inital state for each MC sampling cycle 
+				elif (line[0:2] == 'MC'):
+					i_MC = int(line.split(':')[1])
+
+					#print microstate after each monte carlo sampling
+					if i_MC > 0:
+						print('End microstate of {}-th Monte Carlo sampling: {}'.format(i_MC-1, state))
+
+					line = in_file.readline()
+					while (len(line.rstrip()) ==0 or line[0] == '#'): 
+						#print('delete',line)
+						line = in_file.readline()
+					n_lines += 1
+					tmp, free_conf_id_str = line.rstrip().split(':')
+					if (n_free != int(tmp)):
+						raise ValueError('n_free value not match to record in fourth line')
+					state = list(map(int,free_conf_id_str.split()))
+					print("Start microstate of {}-th Monte Carlo sampling: {}".format(i_MC, state))
+
+					if (len(state) != n_free):
+						raise ValueError('number of conf in initial state must equal to n_free value')
+
+				#read each flips for every MC sampling
+				else:
+					energy, count, flips_str=line.split(',')
+					energy, count=float(energy), int(count)
+					new_confs = list(map(int, flips_str.split()))
+					for conf in new_confs:
+						state[conf_res_inter[conf]]=conf
+						#print(conf)
+					#print(state)
+					if energy < lowest_E:
+						lowest_E = energy
+						lowest_state['free_conf_ids'] = state
+
+
+				line = in_file.readline()
+			print('End microstate of {}-th Monte Carlo sampling: {}'.format(i_MC,state))
+			print('\nState with lowest energy Emin {:.2f} is:\nFixed_conf_ids: {}\nFree_conf_ids: {}'\
+				.format(lowest_E, lowest_state['fixed_conf_ids'], lowest_state['free_conf_ids']))
+
+
 
 if __name__ == '__main__':
 	if len(sys.argv) < 3:
@@ -278,6 +386,8 @@ if __name__ == '__main__':
 		ms_states = read_ms_processor.read_old_ms(file_path=file)
 	elif version ==  'new_version':
 		ms_states = read_ms_processor.read_ms(file_path= file)
+	elif version == 'Stable-MCCE':
+		read_ms_processor.read_new_ms(file_path= file)
 	else: 
 		ms_states = read_ms_processor.read_re_ms(file_path=file)
 	print("Microstate movements: {0}".format(len(ms_states)))
